d0c07b397df44859d3ef408b1eca6338e58dc663
Бекназар Эсенбаев <esenbaevbeknazar@gmail.com>
2022-12-25 10:11:02 +0600

    knsnd,mnfs,mdnfsdf

---
+++ lesson_029/client/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+const   box = document.getElementById('box'),
+        btns = document.getElementsByTagName('button'),
+        circles = document.getElementsByClassName('circle'),
+        hearts = document.querySelectorAll('.heart'),
+        oneHeart = document.querySelector('.heart');
+
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+
+
+box.style.cssText = 'background-Color: blue; width: 500px;';
+
+
+btns[1].style.borderRadius = '100%';
+
+circles[0].style.backgroundColor = 'red';
+
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+
+
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+
+const div  = document.createElement('div');
+
+// const text = document.createTextNode('тут был я');
+
+div.classList.add('black');
+
+
+
+
