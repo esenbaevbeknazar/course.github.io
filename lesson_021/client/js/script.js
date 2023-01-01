2823f566c430934036c24310cca75824497362c5
beknazar <esenbayev92@inbox.ru>
2023-01-01 16:15:40 +0600

    sjhajksd

--- lesson_021/client/js/script.js
+++ lesson_021/client/js/script.js
@@ -1,44 +1,25 @@
 "use strict";
 
+const  arr = [1,2,3,6,8];
 
-const obj = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function(){
-        console.log("Test");
-    }
-};
+// arr.pop();
+// arr.push(10);
 
-obj.makeTest();
+// console.log(arr);
 
-const {border, bg} = obj.colors;
-console.log(border);
+arr.forEach(function(item, i, arr){
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
 
-
-console.log(Object.keys(obj).length);
-
-// console.log(obj.name);
-
-// delete obj.name;
-
-// console.log(obj);
-
-// let counter =0;
-// for (let key in obj) {
-//     if (typeof(obj[key]) === 'object'){
-//         for (let i in obj[key]) {
-//             console.log(`Свойства ${i}, имеет значение ${obj[key][i]}`);
-//             counter++;
-//         }
-//     } else {
-//         console.log(`Свойства ${key}, имеет значение ${obj[key]}`);
-//         counter++;
-//     }
-    
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
 // }
-// console.log(counter);
+
+for (let value of arr){
+    console.log(value);
+}
+
+
+const str = prompt (", ");
+const products = str.split(", ");
+console.log(products.join('; '));
